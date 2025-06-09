62ff1f0bbd7bcfa07bcefd219248322425727ad9
Nicolas Deroo <42850170+deroonicolas@users.noreply.github.com>
2025-06-09 21:28:50 +0200

    voiture

--- src/main/java/parfait/object/Voiture.java
+++ src/main/java/parfait/object/Voiture.java
@@ -5,18 +5,38 @@ public class Voiture {
 	private String name;
 	private String brand;
 
+	/**	
+	 * Returns the name
+	 * 
+	 * @return
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets the name
+	 * 
+	 * @param name The car name
+	 */
 	public void setName(String name) {
-		this.name = name;	
+		this.name = name;
 	}
 
+	/**
+	 * Returns the brand
+	 * 
+	 * @return The brand
+	 */
 	public String getBrand() {
 		return brand;
 	}
 
+	/**
+	 * Sets the brand
+	 * 
+	 * @param brand The brand
+	 */
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
