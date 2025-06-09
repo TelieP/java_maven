9b7cd04277f742db3c030c5a7e307c2cb27d76c4
Nicolas Deroo <42850170+deroonicolas@users.noreply.github.com>
2025-06-09 21:18:30 +0200

    voiture

---
+++ src/main/java/parfait/object/Voiture.java
@@ -0,0 +1,24 @@
+package parfait.object;
+
+public class Voiture {
+
+	private String name;
+	private String brand;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+}
